38013daf54c78cb6c3bf192824363752e9e18c94
Abu-Huraira <shamsbd71@users.noreply.github.com>
2016-06-01 18:08:52 +0600

    error handler added

--- assets/script.js
+++ assets/script.js
@@ -7,24 +7,27 @@
  *
  */
 
-(function($){
+(function($) {
 
-    $(document).ready(function(){
+    $(document).ready(function() {
         $('#xcon-form').submit(function() {
-           var value   = $(this).serializeArray();
-           $.ajax({
-               type   : 'POST',
-               data   : value,
-               beforeSend: function(){
-                   $('#xcon-submit').addClass('loading');
-               },
-               success: function (response) {
-                   $('#xcon-msg').fadeOut().html(response).fadeIn().delay(2000);
-                   $('#xcon-submit').removeClass('loading');
-               }
-           });
-           return false;
-       });
+            var value = $(this).serializeArray();
+            $.ajax({
+                type: 'POST',
+                data: value,
+                beforeSend: function() {
+                    $('#xcon-submit').addClass('loading');
+                },
+                error: function(response) {
+                    console.log(response);
+                },
+                success: function(response) {
+                    $('#xcon-msg').fadeOut().html(response).fadeIn().delay(2000);
+                    $('#xcon-submit').removeClass('loading');
+                }
+            });
+            return false;
+        });
     });
 
 })(jQuery);
