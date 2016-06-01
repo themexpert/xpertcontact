03067e8ce7776369b123769558bed6776a03727a
Abu-Huraira <shamsbd71@users.noreply.github.com>
2016-06-01 17:03:48 +0600

    error info position moved to button top

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Xpert Contact
- * @version ##VERSION##
+ * @version 1.1.1
  * @author ThemeXpert http://www.themexpert.com
  * @copyright Copyright (C) 2009 - 2011 ThemeXpert
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
@@ -14,33 +14,30 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 // Load jQuery
 JHtml::_('jquery.framework');
 ?>
-
 <div id="xcon-<?php echo $module->id; ?>" class="xpert-contact">
-
-    <div id="xcon-msg"></div>
-
     <form id="xcon-form">
-      <?php
-      foreach ($fieldSets as $name => $fieldSet) :
-        foreach ($form->getFieldset($name) as $field):
-          ?>
-          <div>
-            <?php echo $field->label; ?>
-            <?php echo $field->input; ?>
-          </div>
-          <?php
-        endforeach;
-      endforeach;
-      ?>
-
+        <?php
+            foreach ($fieldSets as $name => $fieldSet) :
+                foreach ($form->getFieldset($name) as $field):
+        ?>
+                    <div>
+                        <?php echo $field->label; ?>
+                        <?php echo $field->input; ?>
+                    </div>
+        <?php
+                endforeach;
+            endforeach;
+        ?>
+        
+        <div id="xcon-msg"></div>
         <div>
-            <button id="xcon-submit" type="submit" class="btn btn-primary"><?php echo JText::_('SEND');?></button>
+                <button id="xcon-submit" type="submit" class="btn btn-primary">
+                        <?php echo JText::_('SEND');?>
+                </button>
         </div>
         <input type="hidden" name="option" value="com_ajax" />
         <input type="hidden" name="module" value="xpertcontact" />
         <input type="hidden" name="format" value="raw" />
         <?php echo JHtml::_('form.token'); ?>
-
     </form>
-
 </div>
